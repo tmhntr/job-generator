46924dc9c38f8dd7c0b066432a7e91a23ca7de99
Tim Hunter <thunte27@uwo.ca>
2023-03-09 04:15:30 -0500

    fix(core): add exception handling for posting

--- src/core/posting.py
+++ src/core/posting.py
@@ -3,31 +3,41 @@ import pyperclip as pc
 
 class Posting:
     def __init__(self):
-        self.name: str = ""
-        self.text: str = ""
+        self.name = None
+        self.text = None
 
     def get_text_from_clipboard(self):
         return pc.paste()
     
-    def set_text(self, text):
+    def set_text(self, text: str):
         self.text = text
 
-    def get_text(self):
-        return self.text
+    # def get_text(self):
+    #     return self.text
     
     def get_name(self):
+        if self.name is None:
+            raise Exception("Posting name is not set")
         return self.name
     
-    def set_name(self, name):
+    def set_name(self, name: str):
         self.name = name
     
-    def compile(self):
-        pass
+    def get_text_from_clipboard(self):
+        self.text = pc.paste()
+        return self.text
+    
+    def get_name_from_input(self):
+        self.name = input("Enter the company/job name: ")
+        return self.name
+
+    def get_text_from_input(self):
+        self.text = input("Enter the posting text: ")
+        return self.text
         
-    def get_str(self):
-        if hasattr(self, 'text') and self.text:
-            return self.text
-        self.text = self.get_text_from_clipboard()
+    def get_text(self):
+        if self.text is None:
+            raise Exception("Posting text is not set")
         return self.text
         
             
